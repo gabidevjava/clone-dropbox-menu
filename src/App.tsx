62549e0da99a38cfa351e583d9da765ae6465a0a
Gabriela Guedes <zooiv3rde@gmail.com>
2020-08-14 21:31:35 -0300

    add primeira section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import GlobalStyles from './styles/GlobalStyles';
+import Section from './components/Section';
 
 function App() {
   return (
     <>
-      <h1>Gaby</h1>
+     <Section />
 
       <GlobalStyles />
     </>
