41594a73fb80d48e53bc0bd32ec9f5d489db355c
Gabriela Guedes <zooiv3rde@gmail.com>
2020-08-14 21:19:28 -0300

    add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import GlobalStyles from './styles/GlobalStyles';
+
 function App() {
   return (
-    <h1>Gaby</h1>
+    <>
+      <h1>Gaby</h1>
+
+      <GlobalStyles />
+    </>
   );
 }
 
