932968fafc46ed06d524f2fa7898aa5d52743193
Gabriela Guedes <zooiv3rde@gmail.com>
2020-08-18 15:42:01 -0300

    estilizando page

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Container, Content } from './styles';
+import { Container, HeaderWrapper, Header, DropboxLogo, Content } from './styles';
 
 interface Props {
     variant: 'blue' | 'beige' | 'white' | 'black';
@@ -11,6 +11,16 @@ interface Props {
 const Section: React.FC<Props> = ({variant, title, description}) => {
     return (
         <Container className={variant}>
+            <HeaderWrapper>
+                <Header>
+                    <h1>
+                      <DropboxLogo /> 
+                      <span>Dropbox</span>
+                    </h1>
+                    <button>Interagir</button>
+                </Header>
+            </HeaderWrapper>
+
             <Content>
                 <h2>{title}</h2>
                 <p>{description}</p>
